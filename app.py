f17eb5c7843a19c735050bab6fc5898fbb747af2
woacwoak <woacwoak@gmail.com>
2025-11-20 16:15:45 +0100

    I added routing between pages (from main to signup) and added some css and html

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, redirect
-
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return "testing 123"
+    return render_template("index.html")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    return render_template("signup.html")
 
 @app.route("/login")
 def login():
-    return "login"
+    return render_template("login.html")
 
 
 
